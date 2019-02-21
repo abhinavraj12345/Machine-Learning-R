754358154ec593fff5d42431a51f24bdbde1bca8
Abhinav Raj <46519176+abhinavraj12345@users.noreply.github.com>
2019-02-22 01:28:34 +0530

    Update kfold.R

--- kfold.R
+++ kfold.R
@@ -3,11 +3,11 @@ fold=16
 #-------------------------------------------------------------
 test=NULL
 train=NULL
-d=floor(nrow(iris)/fold)
+d=floor(nrow(data)/fold)
 k=1:d
 for(i in 1:fold){
-  test[[i]]=iris[k,]
-  train[[i]]=iris[-k,]
+  test[[i]]=data[k,]
+  train[[i]]=data[-k,]
   k=d+k
 }
 #-------------------------------------------------------------

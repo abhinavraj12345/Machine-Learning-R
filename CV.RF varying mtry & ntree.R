930b15b4df1c2974b081f3ef68b63ca12dc6f2bb
Abhinav Raj <46519176+abhinavraj12345@users.noreply.github.com>
2019-02-22 15:33:33 +0530

    Create CV.RF varying mtry & ntree.R

---
+++ CV.RF varying mtry & ntree.R
@@ -0,0 +1,41 @@
+#---------------------- reading data -----------------------------
+library(randomForest)
+data1=read.csv("C:\\Users\\Abhinav Raj\\Downloads\\Data sets\\breast-cancer.csv",header = T)
+#--------------------data cleaning--------------------------------
+cdata=data1[,1:32]
+fdata= cdata[,-1]#--------------------------------------final data
+data<-fdata
+fold=5
+#-----------------cross validation random forest-------------------
+CV.random.forest<-function(data,fold,a,b){ #other function parameters){
+  d=floor(nrow(data)/fold)
+  k=1:d
+  train<-NULL
+  test<-NULL
+  accuracy=NULL
+  for(i in 1:fold){
+    test=data[k,]
+    train=data[-k,]
+    cancer.rf=randomForest(formula=diagnosis~.,data=train,mtry=a,ntree=b)#include function parameters 
+    p=predict(cancer.rf,test)
+    accuracy[i]=mean(test$diagnosis==p)
+    k=d+k
+  }#END:for
+  return(mean(accuracy))
+}
+#-------------------grid points----------------------------------
+
+x<-seq(2,30,by=2)
+y<-seq(50,500,by=50)
+aa=a=matrix(NA,nrow=length(x),length(y))
+for(i in 1:length(x)){
+  for(j in 1:length(y)){
+    aa[i,j]=CV.random.forest(data,fold,x[i],y[j])
+  }#END:j
+}#END:i
+accuracy.matrix=t(aa)
+#---------------printing accuracy matrix--------------------------
+print(accuracy.matrix)
+l=which(accuracy.matrix == max(accuracy.matrix), arr.ind = TRUE)
+cat(sprintf("\n  accuracy is %f for mtry = %d and ntree = %d in cross-validation \n \n",
+            max(accuracy.matrix),x[l[1,2]],y[l[1,1]]))
